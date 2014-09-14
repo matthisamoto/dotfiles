7626e6e50f299eb85c69bba5a418362cd7b43c53
Brett Buddin <brett@buddin.us>
2014-09-14 14:22:14 -0400

    gpush

--- zsh/modules/git.zsh
+++ zsh/modules/git.zsh
@@ -5,3 +5,4 @@ alias gbranch='git branch'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset%C(yellow)%d%Creset%Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gcommit='git commit'
 alias gamend='git commit -a'
+alias gpush='git push'
