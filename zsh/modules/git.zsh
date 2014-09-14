e65d55c7c30fdfd3d429d33479f411a03db2725d
Brett Buddin <brett@buddin.us>
2014-09-14 15:11:03 -0400

    Adjusting aliases and formatting in git

--- zsh/modules/git.zsh
+++
@@ -1,8 +0,0 @@
-alias gstatus='git status -sb'
-alias gadd='git add'
-alias gdiff='git diff'
-alias gbranch='git branch'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset%C(yellow)%d%Creset%Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gcommit='git commit'
-alias gamend='git commit -a'
-alias gpush='git push'
